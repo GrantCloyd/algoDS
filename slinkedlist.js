26cdd4bdf90e396a4fcfbaf600c40bfd9b2f4839
Grant Cloyd <dgcloyd@gmail.com>
2021-09-17 17:20:18 -0500

    Added insertNode, removeNode, reverse

--- slinkedlist.js
+++ slinkedlist.js
@@ -23,6 +23,7 @@ class SingleLinkedList {
          this.head = node
       }
       this.length += 1
+      return this.head
    }
 
    push(val) {
@@ -36,6 +37,7 @@ class SingleLinkedList {
          this.tail = node
       }
       this.length += 1
+      return this.tail
    }
 
    shift() {
@@ -97,23 +99,73 @@ class SingleLinkedList {
       node.value = updatedValue
       return node
    }
+
+   insertNode(location, value) {
+      if (location === this.length) return this.push(value)
+      if (location === 0) return this.unshift(value)
+      const insertNode = new Node(value)
+      const nodeBeforeInsert = this.getNode(location - 1)
+      if (!nodeBeforeInsert) return undefined
+      insertNode.next = nodeBeforeInsert.next
+      nodeBeforeInsert.next = insertNode
+      this.length++
+      return insertNode
+   }
+
+   removeNode(location) {
+      if (location === this.length - 1) return this.pop()
+      if (location === 0) return this.shift()
+      const prevNode = this.getNode(location - 1)
+      if (!prevNode || !prevNode.next.next) return undefined
+      const removedNode = prevNode.next
+      prevNode.next = removedNode.next
+      this.length--
+      return removedNode
+   }
+
+   reverse() {
+      if (!this.head) return undefined
+      if (this.head === this.tail) return this
+      let temp = this.head
+      this.head = this.tail
+      this.tail = temp
+      let prevNode = null
+      let nextNode = null
+      for (let i = 0; i < this.length; i++) {
+         nextNode = temp.next
+         temp.next = prevNode
+         prevNode = temp
+         temp = nextNode
+      }
+      return list
+   }
 }
 
 const list = new SingleLinkedList()
-list.unshift(18)
-list.unshift(13)
-list.push(17)
-list.push(24)
-
-list.unshift(2)
+list.unshift(18) //18
+list.unshift(13) // 13, 18
+list.unshift(2) // 2, 13, 18
+list.push(17) // 2, 13, 18, 17
+list.push(24) // 2, 13, 18, 17, 24
 list.push(10)
 
-console.log(list.head)
-console.log(list.tail)
+//should be 2,13,18,17, 24, 10, length of 6
+console.log(list)
+
+list.getNode(3) // 17
+list.setNode(3, 100) // 17 => 100
+list.insertNode(2, 35) // 35 inserted before 18 and then 35 point to 18
+list.removeNode(2) // 35 is gone, 18 points to 100
+list.insertNode(0, 55) // 55 points to 2
+console.log("\n---- STARTING LIST DISPLAY ----\n")
+for (let i = 0; i < list.length; i++) {
+   console.log(list.getNode(i))
+}
 console.log(list.length)
-console.log(list)
-console.log(list.pop())
-console.log(list.shift())
-console.log(list)
-console.log(list.getNode(3))
-console.log(list.setNode(-1, 100))
+
+// should be 55, 2, 13, 18, 100, 24, 10 length of 7
+list.reverse()
+console.log("\n---- STARTING REVERSE DISPLAY ----\n")
+for (let i = 0; i < list.length; i++) {
+   console.log(list.getNode(i))
+}
