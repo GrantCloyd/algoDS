3d99eb4e44f43bd4abd9d6b8c44daba09879a396
Grant Cloyd <dgcloyd@gmail.com>
2021-09-20 16:13:45 -0500

    Doubly Linked List with shift, unshift, get, set, insert, and light testing

--- dblLinkedList.js
+++ dblLinkedList.js
@@ -1,6 +1,6 @@
 class Node {
-   constructor(val) {
-      this.value = val
+   constructor(value) {
+      this.value = value
       this.next = null
       this.prev = null
    }
@@ -41,30 +41,116 @@ class DblLinkedList {
       this.length--
       return temp
    }
+
+   shift() {
+      if (!this.head) return false
+      let temp = this.head
+      if (this.head === this.tail) {
+         this.head = null
+         this.tail = null
+      } else {
+         this.head = this.head.next
+         this.head.prev = null
+         temp.next = null
+      }
+      this.length--
+      return temp
+   }
+
+   unshift(val) {
+      const node = new Node(val)
+      if (!this.head) {
+         this.head = node
+         this.tail = node
+      } else {
+         node.next = this.head
+         this.head.prev = node
+         this.head = node
+      }
+      this.length++
+      return this
+   }
+
+   get(index) {
+      if (!this.head) return undefined
+      if (this.length - 1 < index || index < 0) return undefined
+      let current = null
+      if (index <= this.length / 2) {
+         current = this.head
+         for (let i = 0; i < index; i++) {
+            current = current.next
+         }
+      } else {
+         current = this.tail
+         for (let i = this.length - 1; i > index; i--) {
+            current = current.prev
+         }
+      }
+      return current
+   }
+
+   set(index, value) {
+      const updateNode = this.get(index)
+      if (!updateNode) return undefined
+      updateNode.value = value
+      return updateNode
+   }
+
+   insert(index, value) {
+      const newNode = new Node(value)
+      if (this.length < index || index < 0) return undefined
+      if (index === 0) return this.unshift(newNode)
+      if (index === this.length) return this.push(newNode)
+      let prevLocationNode = this.get(index)
+      newNode.next = prevLocationNode
+      newNode.prev = prevLocationNode.prev
+      prevLocationNode.prev.next = newNode
+      prevLocationNode.prev = newNode
+      this.length++
+      return this
+   }
 }
 
 const list = new DblLinkedList()
 
-let i = 5
+let i = 8
 
 while (i > 0) {
-   list.push(Math.floor(Math.random() * 100))
+   list.unshift(Math.floor(Math.random() * 100))
    i--
 }
 
-console.log("----PRINTING PUSH-----")
+console.log("----PRINTING PUSH/Unshift-----")
 let current = list.head
+let count = 0
 while (current) {
-   console.log(current)
+   console.log(count, current)
    current = current.next
+   count++
 }
 
-console.log("Final list:", list)
+let j = 3
 
-console.log("---- Printing POP ----")
-while (list.tail) {
-   console.log(list.tail)
-   list.pop()
+while (j > 0) {
+   let randInd = Math.floor(Math.random() * i)
+   let randVal = Math.floor(Math.random() * 100)
+   list.insert(randInd, randVal)
+   j--
 }
 
-console.log(list)
+// console.log("---- Printing POP/Shift ----")
+// while (list.head) {
+//    console.log(list.head)
+//    list.shift()
+// }
+
+// console.log(list)
+
+current = list.head
+count = 0
+console.log("------After insert----")
+while (current) {
+   console.log(count, current)
+   current = current.next
+   count++
+}
